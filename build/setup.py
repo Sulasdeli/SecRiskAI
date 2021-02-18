ad6e443e420153900ac02f7e8af413599302605a
Sulasdeli <erion.sula@uzh.ch>
2021-02-18 23:24:00 +0100

    further improvements

--- build/setup.py
+++ build/setup.py
@@ -28,9 +28,9 @@ except Exception:
 
 setuptools.setup(
     name='RiskClassifier',
-    version='20210218230338_89EAF4',
+    version='20210218232351_9FEE67',
     description="BentoML generated model module",
-    long_description="""# Generated BentoService bundle - RiskClassifier:20210218230338_89EAF4
+    long_description="""# Generated BentoService bundle - RiskClassifier:20210218232351_9FEE67
 
 This is a ML Service bundle created with BentoML, it is not recommended to edit
 code or files contained in this directory. Instead, edit the code that uses BentoML
