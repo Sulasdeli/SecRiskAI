f2eb4d6fd1d4d909147a9f59281040475afefa82
Sulasdeli <erion.sula@uzh.ch>
2021-05-06 11:46:01 +0200

    fixes

--- frontend/src/helpers/Capitalize.tsx
+++ frontend/src/helpers/Capitalize.tsx
@@ -1,5 +1,5 @@
 export const capitalize = (s: string) => {
-    if (s.length <= 3) return s
+    if (s === "DNS" || s === "SSL") return s
     let result = s.toLowerCase().split(" ")
     return result.map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(" ")
 }
