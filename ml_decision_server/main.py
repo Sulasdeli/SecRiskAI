a27c3358fac418f819704a5c966c2165f8bbc12e
Sulasdeli <erion.sula@uzh.ch>
2021-05-29 17:29:43 +0200

    ddos model improvements

--- ml_decision_server/main.py
+++ ml_decision_server/main.py
@@ -9,31 +9,31 @@ if __name__ == "__main__":
     # Overall Cyberattack Prediction Artifacts
     risk_classifier_service.pack('knn_model', load('src/models/KNN_classifier.joblib'),
                                  metadata={
-                                     'accuracy': '96.59%',
-                                     'n_neighbors': 28,
+                                     'accuracy': '96.40%',
+                                     'n_neighbors': 23,
                                      'weights': 'distance'
                                  })
     risk_classifier_service.pack('mlp_model', load('src/models/MLP_classifier.joblib'),
                                  metadata={
-                                     'accuracy': '99.57%',
+                                     'accuracy': '99.63%',
                                      'activation': 'tanh',
-                                     'hidden_layer_sizes': '(5, 2)',
-                                     'learning_rate': 'constant',
+                                     'hidden_layer_sizes': '(5, 5)',
+                                     'learning_rate': 'invscaling',
                                      'solver': 'adam'
                                  })
     risk_classifier_service.pack('svm_model', load('src/models/SVM_classifier.joblib'),
                                  metadata={
-                                     'accuracy': '99.30%',
+                                     'accuracy': '99.25%',
                                      'gamma': 'scale',
                                      'kernel': 'rbf'
                                  })
     risk_classifier_service.pack('tree_model', load('src/models/Tree_classifier.joblib'),
                                  metadata={
-                                     'accuracy': '93.15%',
+                                     'accuracy': '92.17%',
                                      'criterion': 'gini',
                                      'max_depth': 9,
                                      'min_samples_leaf': 5,
-                                     'min_samples_split': 5
+                                     'min_samples_split': 2
                                  })
     risk_classifier_service.pack('scaler', load('src/scaler/MinMaxScaler.joblib'),
                                  metadata={
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     # DDoS Prediction Artifacts
     risk_classifier_service.pack('ddos_mlp_model', load('src/models/MLP_ddos_classifier.joblib'),
                                  metadata={
-                                     'accuracy': '90.03%',
+                                     'accuracy': '89.21%',
                                      'activation': 'logistic',
                                      'hidden_layer_sizes': '(5, 2)',
                                      'learning_rate': 'constant',
